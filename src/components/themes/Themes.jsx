88f79c8b0dee2322cf937a931f94783918fecc19
SoumyadiptoPal <soumyadiptocodes@gmail.com>
2023-10-06 20:07:27 +0530

    Updated themesData.js

--- src/components/themes/Themes.jsx
+++ src/components/themes/Themes.jsx
@@ -5,7 +5,7 @@ export default function Theme() {
     <section class="grid">
       {themesData.map((el) => (
         <div class="theme_item">
-          <div className="icon">{el.themeImageURL}</div>
+          <div className="icon"><img src={el.themeImageURL} width="100" alt="Icon"/></div>
           <br />
           <div class=""><strong style={{fontSize:"larger"}}>{el.themeName}</strong></div>
           <div class="">{el.themeDescription}</div>
