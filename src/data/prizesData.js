4e7e1944f67d90c2397f04a1a9804df285902f80
SoumyadiptoPal <soumyadiptocodes@gmail.com>
2023-10-06 20:31:40 +0530

    Updated prizesData.js

---
+++ src/data/prizesData.js
@@ -0,0 +1,37 @@
+export const PrizeData = [
+  {
+    icon: './icons/goldTrophy.svg',
+    type: "Overall First",
+    content:
+      "The first overall prize will be given to a project that outstands all other submissions.",
+  },
+  {
+    icon: './icons/silverTrophy.svg',
+    type: "Overall Second",
+    content:
+      "The second overall prize will be given to the second-best project in the hackathon.",
+  },
+  {
+    icon: './icons/bronzeTrophy.svg',
+    type: "Overall Third",
+    content:
+      "The third overall prize will be given to the third-best project in the hackathon.",
+  },
+  {
+    icon: './icons/trophy.svg',
+    type: " Best Women Team",
+    content:
+      "Your project will qualify for this category if your all team members are women.",
+  },
+  {
+    icon: './icons/trophy.svg',
+    type: "Best DDU Team",
+    content:
+      "Your project will qualify for this category if your all team members are from DDU.",
+  },
+  {
+    icon:'./icons/trophy2.svg',
+    type: "More prizes",
+    content: "More prizes will be revealed later",
+  },
+];
