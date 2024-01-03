de99025bd032b17399d0e7228ccabe30eb5e9362
SoumyadiptoPal <soumyadiptocodes@gmail.com>
2024-01-03 12:36:18 +0530

    Added Backdrop and disable scroll when side drawer is opened. used JS to toggle the side drawer as using only CSS will be length and complex.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -16,10 +16,6 @@ const navbarLinks = [
     name: "Sponsors",
     icon: '/icons/Sponsor.svg'
   },
-  /*{
-    link: "#teams",
-    name: "Teams",
-  },*/
   {
     link: "#faqs",
     name: "FAQs",
@@ -30,6 +26,14 @@ const navbarLinks = [
 const Navbar = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
   const toggleNav = () => {
+    if(!isNavOpen)
+    {
+      document.querySelector('body').className='h-full overflow-y-hidden md:overflow-y-scroll';
+    }
+    else
+    {
+      document.querySelector('body').className='';
+    }
     setIsNavOpen(!isNavOpen);
   };
 
@@ -37,9 +41,12 @@ const Navbar = () => {
     <nav
       className="flex flex-row justify-between sticky top-0 z-50 items-center py-2.5 px-8 md:px-14 bg-gray-900"
     >
-      <div className="flex items-center justify-center h-full font-semibold text-white">
+      <div className="flex items-center justify-center h-full font-semibold text-white z-30">
         synchronicity - s1
       </div>
+      {isNavOpen && (
+        <div className="fixed md:hidden top-0 left-0 w-full h-full bg-black opacity-50 z-10"></div>
+      )}
       <ul className="hidden md:flex flex-row items-center justify-center h-12 text-gray-200 list-none">
         {navbarLinks.map((el) => (
           <li className="p-4" key={el.link}>
@@ -59,7 +66,7 @@ const Navbar = () => {
       <ul
         className={`fixed md:hidden bg-gray-900 top-0 ${
           isNavOpen ? "right-0" : "right-[-300px]"
-        } w-3/5 h-screen pt-14 text-white`}
+        } w-3/5 h-screen pt-14 text-white z-50`}
       >
         <button className="text-3xl absolute top-6 right-6" onClick={toggleNav}>
           &#10006;
@@ -69,7 +76,6 @@ const Navbar = () => {
             <a href={el.link}>
               <div className="flex flex-row items-center">
                 <img src={el.icon} alt={el.name} className="h-6 mr-2"/>
-                {/* <span className="h-6">{el.icon}</span> */}
                 <span className="links">{el.name}</span>
               </div>
             </a>
