0c7a0447089ed53bb2d8b60891fa8bc6410d84cf
Manosrija Nasrin <manosrijanasrin@gmail.com>
2024-01-03 09:29:38 +0530

    FIX: Added fortawesome dependencies and fixed icons in the footer

--- src/components/media/Media.jsx
+++ src/components/media/Media.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { SOCIALS } from "../../Module/General";
+import socialData from "../../data/socialData";
 
 import "./Media.css";
 
@@ -8,32 +7,32 @@ export default function Media() {
     <div className="media">
       <p>Follow us on social media for updates</p>
       <div className="single">
-        <a rel="noreferrer" target="_blank" href={SOCIALS.instagram}>
+        <a rel="noreferrer" target="_blank" href={socialData.instagram}>
           <i className="fab fa-3x fa-instagram"></i>
         </a>
       </div>
 
-      <div className="single">
-        <a rel="noreferrer" target="_blank" href={SOCIALS.discord}>
+      {/* <div className="single">
+        <a rel="noreferrer" target="_blank" href={socialData.discord}>
           <i className="fab fa-3x fa-discord"></i>
         </a>
-      </div>
+      </div> */}
 
       <div className="single">
-        <a rel="noreferrer" target="_blank" href={SOCIALS.linkedin}>
+        <a rel="noreferrer" target="_blank" href={socialData.linkedin}>
           <i className="fab fa-3x fa-linkedin-in"></i>
         </a>
       </div>
-      <div className="single">
-        <a rel="noreferrer" target="_blank" href={SOCIALS.twitter}>
+      {/* <div className="single">
+        <a rel="noreferrer" target="_blank" href={socialData.twitter}>
           <i className="fab fa-3x fa-twitter"></i>
         </a>
-      </div>
-      <div className="single">
-        <a rel="noreferrer" target="_blank" href={SOCIALS.devpost}>
+      </div> */}
+      {/* <div className="single">
+        <a rel="noreferrer" target="_blank" href={socialData.devpost}>
           <i className="fab fa-3x fa-dev"></i>
         </a>
-      </div>
+      </div> */}
     </div>
   );
 }
