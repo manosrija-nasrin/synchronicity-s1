c3a0e0b9fd2602ec125ae2236027ef7ae0640e70
Manosrija Nasrin <manosrijanasrin@gmail.com>
2024-01-04 10:14:22 +0530

    TEST: Removed the descriptor property from the sponsor object in data/{partners,genericSponsors}.js

--- src/data/sponsorsData/partners.js
+++ src/data/sponsorsData/partners.js
@@ -1,13 +1,13 @@
 const Partners = [
   {
     name: "beeceptor",
-    descriptor: "API Tooling Partner",
+    // descriptor: "API Tooling Partner",
     sponsorWebsiteURL: "https://beeceptor.com",
     sponsorLogoURL: "/sponsorLogos/beeceptor-logo-horizontal.png",
   },
   {
     name: "unstop",
-    descriptor: "Powered by",
+    // descriptor: "Powered by",
     sponsorWebsiteURL: "https://unstop.com",
     sponsorLogoURL: "/sponsorLogos/unstop-logo.svg",
   },
