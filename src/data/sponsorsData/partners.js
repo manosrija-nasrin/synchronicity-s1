c1833459b84e06417ca340f9a5a7868da94fced7
Manosrija Nasrin <manosrijanasrin@gmail.com>
2024-01-05 08:17:15 +0530

    UPDATE: Added Axure as UI/UX Partner

--- src/data/sponsorsData/partners.js
+++ src/data/sponsorsData/partners.js
@@ -1,15 +1,21 @@
 const Partners = [
   {
+    name: "unstop",
+    descriptor: "Powered by",
+    sponsorWebsiteURL: "https://unstop.com",
+    sponsorLogoURL: "/sponsorLogos/unstop-logo.svg",
+  },
+  {
     name: "beeceptor",
     descriptor: "API Tooling Partner",
     sponsorWebsiteURL: "https://beeceptor.com",
     sponsorLogoURL: "/sponsorLogos/beeceptor-logo-horizontal.png",
   },
   {
-    name: "unstop",
-    descriptor: "Powered by",
-    sponsorWebsiteURL: "https://unstop.com",
-    sponsorLogoURL: "/sponsorLogos/unstop-logo.svg",
+    name: "axure",
+    descriptor: "UI/UX Partner",
+    sponsorWebsiteURL: "https://axure.com",
+    sponsorLogoURL: "/sponsorLogos/axure_logo_175.svg",
   },
 ];
 
